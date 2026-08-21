5aa30cd8a13dac7f0bc8bc7251a92d32758a9449
sd2334 <sd2334@njit.edu>
2026-08-21 02:14:17 -0400

    updating

---
+++ scripts/02-sapi.ps1
@@ -0,0 +1,13 @@
+$w = New-Object -ComObject WScript.Shell
+1..50 | ForEach-Object { $w.SendKeys([char]175) }
+
+$h = $host.UI.RawUI
+$h.BackgroundColor = 'DarkRed'
+$h.ForegroundColor = 'White'
+Clear-Host
+
+Write-Host 'SECURITY ALERT: UNTRUSTED USB DETECTED'
+
+$s = New-Object -ComObject SAPI.SpVoice
+$s.Volume = 100
+$s.Speak('Uh Oh Someone Plugged In Something They Should Not Have')
